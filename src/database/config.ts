01d6d4a6591fe27672cd7dc535f49f2fe4fc5323
SebastianGonzalez <63818852+drixter4@users.noreply.github.com>
2023-12-16 15:51:46 -0600

    update deploy
    update deploy

--- src/database/config.ts
+++ src/database/config.ts
@@ -6,11 +6,13 @@ const database = process.env.DB_NAME || '';
 const user = process.env.DB_USER || '';
 const password = process.env.DB_PASSWORD || '';
 
-const DB = new Sequelize(database, user, password, {
+
+const DB = new Sequelize(database, user, password,
+ {
    host: 'localhost',
    dialect: 'mysql',
    logging: false,
-   port: 3306
+   port: 36012,
 });
 
 export default DB;
