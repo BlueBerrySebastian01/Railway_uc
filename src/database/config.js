2d0ec28c79487c5a66f4933477634b90c95a3f56
SebastianGonzalez <drixter4@gmail.com>
2023-12-18 13:06:02 -0600

    cambio de bd

--- src/database/config.js
+++ src/database/config.js
@@ -8,12 +8,12 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const database =  'railway';
 const user = 'root';
-const password = '5ceC3b4cB5b6eC54Dfe2d-c365AGF346';
+const password = 'cBDd5agHfba1dC6dbgFFgD6DgHB244cH';
 const DB = new sequelize_1.Sequelize(database, user, password, {
-    host: 'roundhouse.proxy.rlwy.net',
+    host: 'monorail.proxy.rlwy.net',
     dialect: 'mysql',
     logging: false,
-    port: '36012'
+    port: '52529'
 });
 exports.default = DB;
 //# sourceMappingURL=config.js.map
