dd725029fb00750ba84f3c49337eab8ff2aba791
SebastianGonzalez <drixter4@gmail.com>
2023-12-18 11:56:54 -0600

    Cambio de bd

--- src/database/config.js
+++ src/database/config.js
@@ -6,14 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const database =  'railway';
-const user = 'root';
-const password = '5ceC3b4cB5b6eC54Dfe2d-c365AGF346';
+const database =  'borcommx_ucback';
+const user = 'borcommx_user_ucback';
+const password = 'hv4DH{%bOQz$';
 const DB = new sequelize_1.Sequelize(database, user, password, {
-    host: 'roundhouse.proxy.rlwy.net',
+    host: 'sh-pro30.hostgator.mx',
     dialect: 'mysql',
     logging: false,
-    port: '36012'
+    port: '2083'
 });
 exports.default = DB;
 //# sourceMappingURL=config.js.map
